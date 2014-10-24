1be779be0894b3ec7019f792367388257e63a12c
THOMAS ENGLAND <thomasengland@THOMASs-iMac.local>
2014-10-23 20:30:52 -1000

    Added functionality for EnterName and changed ViewController layout.

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,9 +11,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var fromLabel: UILabel!
+    @IBOutlet weak var messageTitleLabel: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,8 +32,15 @@ class ViewController: UIViewController {
     @IBAction func sendMailButtonPressed(sender: UIButton) {
         // Code will evaluate when we press the button
         messageLabel.hidden = false
+        fromLabel.hidden = false
+        messageTitleLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
